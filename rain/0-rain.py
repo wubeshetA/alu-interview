709ee15741f079720303e6e3f3a72e77ff12aecb
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-24 23:27:24 +0300

    Add rain

---
+++ rain/0-rain.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""Rain module"""
+
+"""Given a list of non-negative integers representing the heights of walls with unit width 1, as if viewing the cross-section of a relief map, calculate how many square units of water will be retained after it rains.
+
+Prototype: def rain(walls)
+walls is a list of non-negative integers.
+Return: Integer indicating total amount of rainwater retained.
+Assume that the ends of the list (before index 0 and after index walls[-1]) are not walls, meaning they will not retain water.
+If the list is empty return 0."""
+
+def rain(walls):
+    """Calculate how many square units of water will be retained after it rains"""
+    if len(walls) == 0:
+        return 0
+    water = 0
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        water += min(left, right) - walls[i]
+    return water
+
+if __name__ == "__main__":
+    walls = [0, 1, 0, 2, 0, 3, 0, 4]
+    print(rain(walls))
+    walls = [2, 0, 0, 4, 0, 0, 1, 0]
+    print(rain(walls))
